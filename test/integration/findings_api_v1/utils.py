ac753d1155fce54714b2a0d06f45a4b18760e9f4
Sudheesh <skairali@in.ibm.com>
2020-05-29 13:44:52 +0530

    Update utils.py

--- test/integration/findings_api_v1/utils.py
+++ test/integration/findings_api_v1/utils.py
@@ -30,6 +30,10 @@ def read_credentials(separator: str = '=') -> dict:
     """
 
     # read env vars
+    print(os.environ['API_KEY'])
+    print(os.environ['ACCOUNT_ID'])
+    print(os.environ['FINDING_API_ENDPOINT'])
+    print(os.environ['IAM_ENDPOINT'])
     config = {}
     env_keys = ['API_KEY', 'ACCOUNT_ID',
                 'FINDING_API_ENDPOINT', 'IAM_ENDPOINT']
